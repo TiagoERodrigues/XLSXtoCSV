9d1f128a473c4b3619c4dd3317816a65f3dc754b
TiagoERodrigues <40641534+TiagoERodrigues@users.noreply.github.com>
2021-12-14 18:07:05 -0300

    Add files via upload

---
+++ main.py
@@ -0,0 +1,80 @@
+#-*- coding: utf-8 -*-
+import os.path
+
+import pandas as pd
+from tkinter import *
+from tkinter import filedialog
+import csv
+
+def converter():
+
+    with open(r'Conversao.csv', "r", encoding='utf8') as inp, open(r'Planilha.txt', "w", encoding='utf8') as out:
+
+        for file in inp:
+            file = file.replace(",", ";")
+            out.write(file)
+
+    with open(r'Planilha.txt', "r", encoding='utf8') as f1, open(r'Conversao.csv', "w", encoding='utf8') as f2:
+        f1 = pd.read_csv('Planilha.txt')
+        f1.to_csv(r'Conversao.csv', encoding='utf8', index=None)
+        df = pd.DataFrame(pd.read_csv('Conversao.csv', encoding='utf8'))
+
+
+# capture file
+def browseFiles():
+    filename = filedialog.askopenfilename(initialdir= "/", title = "Selecione o arquivo", filetypes=(("XLSX", "*.xlsx*"),("All files", "*.*")))
+    label_file_explorer.configure(text="Arquivo aberto: " + filename)
+    read_file = pd.read_excel(filename)
+    read_file.to_csv(r'Conversao.csv', index=None)
+    df = pd.DataFrame(pd.read_csv(r'Conversao.csv'))
+
+def sair():
+    exit()
+
+if __name__ == '__main__':
+
+    pasta = r'C:\Conversoes'
+    if not os.path.exists(pasta):
+        os.makedirs(pasta)
+    # Create the root window
+    window = Tk()
+
+    # Set window title
+    window.title('XLSX para CSV')
+
+    # Set window size
+    window.geometry("400x200")
+
+    # Set window background color
+    window.config(background="white")
+
+    # Create a File Explorer label
+    label_file_explorer = Label(window,
+                                text="Selecione o arquivo",
+                                width=60, height=4,
+                                fg="blue")
+
+    label_file_tigas = Label(window,
+                                text="Feito por Tiago Rodrigues",
+                                width=60, height=4,
+                                fg="black")
+
+    button_explore = Button(window,
+                            text="Abrir arquivos",
+                            command=browseFiles)
+
+    button_sair = Button(window,
+                            text="Sair",
+                            command=sair)
+
+    button_converter = Button(window,
+                            text="CSV",
+                            command=converter)
+
+    label_file_explorer.grid(column=0, row=1)
+    label_file_tigas.grid(column=0, row=10)
+    button_explore.grid(column=0, row=2)
+    button_converter.grid(column=0, row=7)
+    button_sair.grid(column=0, row=9)
+    # main
+    window.mainloop()
